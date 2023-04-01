5dc6650433fc05853a992619b1853de45c5363dd
Steve Dytewski <dy2ski@gmail.com>
2023-03-31 21:26:23 -0700

    created movielist component

---
+++ src/components/Movielist.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+
+
+function Movielist () {
+    const [name, setName] = useState([]);
+    return (
+    <div className="App">
+       <div>render the data here</div>
+    </div>
+    );
+    }
+
+export default Movielist;
