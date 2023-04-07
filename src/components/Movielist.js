8c243212612ae214eb919b1dc9e52b54f2478bb4
Steve Dytewski <dy2ski@gmail.com>
2023-04-06 17:08:29 -0700

    got movie app to search and display titles

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -5,30 +5,46 @@ import { useState, useEffect } from 'react';
 
 function Movielist () {
     const [name,setName] = useState(['']);
-
-
+    const [searchTerm, setSearchTerm] = useState('');
   
+ 
 
-    const names = async () => {
-        const response = await fetch('http://www.omdbapi.com/?s=her&apikey=32e96066');
+    const names = async (title) => {
+        const response = await fetch('http://www.omdbapi.com/?s='+title +'&apikey=32e96066');
         const data = await response.json();
+
+        // const response = await fetch('http://www.omdbapi.com/?s=her&apikey=32e96066');
         
         console.log(data);
         setName(data.Search);
-          
+        
+         
             
     }
 
         useEffect(() => {
-        names ()
+        names ('her')
     }, [])
 
 
         
     return (
         <div className='container-fluid movie-app'>
+           <div className='search'>
+                <input 
+                    placeholder='Search for moves'
+                    value={searchTerm}
+                    onChange={(e)=> setSearchTerm(e.target.value)}
+                />
+                <button
+                   
+                    alt='search'
+                    onClick={()=> names(searchTerm)}>
+                        Search
+                    </button> 
+            </div>
         <div className='row'>
-        {name.map((movie, index) => <div key={movie[index]}>{movie.Title}</div>)}
+        {name.map((movie, index) => <div key={index}>{movie.Title}</div>)}
        
         </div>
     </div>
