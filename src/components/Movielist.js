2a644fe23db4912fc0e577caa02ac5ff5709fe80
Steve Dytewski <dy2ski@gmail.com>
2023-04-06 17:44:39 -0700

    updated parameter to string displacer with tick marks

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,27 +1,35 @@
 import { useState, useEffect } from 'react';
 
 
-
-
 function Movielist () {
     const [name,setName] = useState(['']);
+    
+    // setting an array empty to use for my movies array
+
     const [searchTerm, setSearchTerm] = useState('');
+    
+    // setting a string to use for my search term
   
  
 
     const names = async (title) => {
-        const response = await fetch('http://www.omdbapi.com/?s='+title +'&apikey=32e96066');
+        const response = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=32e96066`);
         const data = await response.json();
         // const response = await fetch('http://www.omdbapi.com/?s=her&apikey=32e96066');
+
+        //fetching my data from the API and making it dynamic with the title parameter
         
         console.log(data);
         setName(data.Search);
+
+        //using the setName mathod to search my data in an object that contains an array of movies
      
     }
         useEffect(() => {
         names ('her')
     }, [])
 
+    // On page load my movies display the results for her
 
         
     return (
@@ -32,15 +40,22 @@ function Movielist () {
                     value={searchTerm}
                     onChange={(e)=> setSearchTerm(e.target.value)}
                 />
+
+       {/* setting the value to my search and on change calling the method to update the value to what the user types       */}
                 <button
                  
                     alt='search'
                     onClick={()=> names(searchTerm)}>
                         Search
                     </button> 
+
+
+         {/* Calling my function that runs the api call and sets it to what the user searches on button click            */}
             </div>
         <div className='row'>
-        {name.map((movie, index) => <div key={index}>{movie.Title}</div>)}
+        {name.map((movie, index) => <div key={index}> <h1>{movie.Title}</h1> <img src={movie.Poster} width="300" height="300"></img></div>
+        
+        )}
        
         </div>
     </div>
