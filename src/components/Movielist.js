1b2f7179d155dc3a6aa883986a599d2700f4c91f
Steve Dytewski <dy2ski@gmail.com>
2023-11-09 18:52:08 -0800

    importing useRef

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as React from 'react';
 import { set, useForm } from "react-hook-form";
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
