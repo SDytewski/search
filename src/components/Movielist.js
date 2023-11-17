83875cb504156ff2b705a0faf4eaa15c52d562c2
Steve Dytewski <dy2ski@gmail.com>
2023-11-16 17:16:03 -0800

    fixed delete button fade in and out

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -207,15 +207,15 @@ function Movielist() {
 
 
               <div>
-                
+
                 {banner == "" ? <div align="center" id="banner" className="text-white text-4xl p-4 m-5 items-center">Click a movie to add to your favorites</div> :
                   <div className="items-center">
                     <div align="center" id="banner" className="text-black text-4xl p-4 m-5 bg-red-400 items-center">{banner}
-                    {/* </div> */}
-                    
-                  </div>
+                      {/* </div> */}
+
+                    </div>
                   </div>}
-                  <h1 align="center" className="text-white text-4xl p-4">Movies</h1>
+                <h1 align="center" className="text-white text-4xl p-4">Movies</h1>
                 <div className='relative flex items-center'>
                   <div className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft}><MdChevronLeft size={40} color="white" /></div>
                   <div id='slider' className="relative flex items-center w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide">
@@ -224,7 +224,7 @@ function Movielist() {
                       (name.map((movie, index) => (
 
                         <div className="pl-2 pr-2 items-center">
-                          
+
                           <img
                             className="text-white min-w-[160px] min-h-[260px] max-h-[260px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 shrink-0"
                             // component="img"
@@ -236,12 +236,7 @@ function Movielist() {
                               saveMovie(movie.Poster, setStar)
                             }}
                           />
-                          {/* <div className="min-w-1"> */}
-                          {/* <div id="lost"className="text-white break-words overflow-hidden truncate m-5 hover:scale-105 ease-in-out duration-300 shrink-0">{movie.Title}
-                          <div>{movie.Year}</div>
-                          </div> */}
-                          
-                          {/* </div> */}
+
                           {favorites.some(i => i.movPoster.includes(movie.Poster)) ? < div align="center" style={style}> <AiFillHeart /> </div> : (<div align="center" style={style}><TbHeartOff /></div>)}
 
 
@@ -252,9 +247,9 @@ function Movielist() {
 
                       :
                       // <div>
-                        <h4 align="center" className="text-rose-200 text-2xl p-8">
-                          No movies found, search again
-                        </h4>
+                      <h4 align="center" className="text-rose-200 text-2xl p-8">
+                        No movies found, search again
+                      </h4>
                       // </div>
 
                     }
@@ -281,7 +276,7 @@ function Movielist() {
                         {/* <Grid item xs={2}> */}
 
                         <div className="container mx-auto w-1/2 p-1 relative">
-                          <div
+                          <div className="min-w-[160px] min-h-[260px] max-h-[260px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 shrink-0"
                             onMouseEnter={() => showButton(i)}
                             onMouseLeave={hideButton}>
 
@@ -289,7 +284,7 @@ function Movielist() {
                             <img
 
 
-                              className="min-w-[160px] min-h-[260px] max-h-[260px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300 shrink-0"
+                              className="min-w-[160px] min-h-[260px] max-h-[260px] inline-block p-2"
                               component="img"
                               // sx={{ maxHeight: 450, width: '100%', display: 'flex', flexDirection: 'row' }}
                               alt="The house from the offer."
@@ -297,11 +292,20 @@ function Movielist() {
                               onClick={() => { deleteMovie(movs.movPoster, favorites) }}
 
                             />
+                            <div class="absolute w-full h-full top-0 bottom-0 left-0 right-0 flex items-center justify-center">
+                              <div>
+                                <div id="lost" className="cursor">{favorites.Title}
+                                  <div>{favorites.Year}</div>
+                                </div>
 
-                            <button
+                              </div>
 
-                              style={{ maxWidth: '230px', display: see === i ? 'block' : 'none' }} className="cursor-pointer hover:scale-105 ease-in-out duration-300 shrink-0 bg-black text-white font-bold py-2 px-4 border border-white-700 rounded absolute top-2/4 left-2/4 rounded-lg p-4" onClick={() => { deleteMovie(movs.movPoster, favorites) }}>
-                              Delete</button>
+
+                              <button
+
+                                style={{ maxWidth: '230px', display: see === i ? 'block' : 'none' }} className="cursor-pointer hover:scale-105 ease-in-out duration-300 shrink-0 bg-black text-white font-bold border border-white-700 rounded absolute rounded-lg p-3" onClick={() => { deleteMovie(movs.movPoster, favorites) }}>
+                                Delete</button>
+                            </div>
 
                           </div>
                         </div>
