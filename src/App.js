011f6599cad32eeacab9c04fe0b08ccf37de9b3c
Steve Dytewski <dy2ski@gmail.com>
2023-12-04 18:19:29 -0800

    created Favorites.js component, still need to pass hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Starwars from './components/Starwars.js'
 import Movielist from './components/Movielist.js'
+import Favorites from './components/Favorites.js'
 
 
 function App() {
   return (
     <div className="App">
       <Movielist />
+      <Favorites />
       {/* <Movielist /> */}
     </div>
   );
