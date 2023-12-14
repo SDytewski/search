c684047722af7538fe60e0554c43775f0f488347
Steve Dytewski <dy2ski@gmail.com>
2023-12-13 19:52:40 -0800

    moving favorites and other states to parent component and removing them from movielist.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Favorites from './components/Favorites.js'
 import { useState, useEffect, useRef } from 'react';
 
 
+
 function App() {
+
+
+
   const slideFavoriteLeft = () => {
     var slider = document.getElementById('sliderFav')
     slider.scrollLeft = slider.scrollLeft - 500;
@@ -18,14 +22,50 @@ function App() {
   }
 
   const [favorites, setFavorites] = useState([]);
+  const [banner, setBanner] = useState("");
+  const [star, setStar] = useState(null);
 
   useEffect(() => {
     // console.log(favorites);
   }, [favorites])
 
+  const saveMovie = (poster, title) => {
+
+    const r = favorites.some(i => i.movPoster.includes(poster, title));
+    // console.log("Hello");
+
+    // (!favorites[item].movPoster === yolo) 
+    if (!r) {
+      setBanner("Movie added!")
+      setTimeout(() => setBanner(""), 2000);
+      setFavorites((film) => {
+        const shows = {
+          id: film.length === 0 ? 1 : film[film.length - 1].id + 1,
+          movPoster: poster,
+          movTitle: title
+        }
+        // alert("movie added to favorites!")
+        // console.log(shows.movTitle);
+        // console.log(shows.movPoster);
+        // console.log(shows);
+        //  setStar(shows.movPoster);
+
+        const returnValue = [...film, shows];
+        return returnValue;
+
+
+      });
+    }
+    else {
+      setBanner("Movie already exists in your favorites")
+      setTimeout(() => setBanner(""), 2000);
+    }
+
+  }
+  
   return (
     <div className="App" style={{ backgroundSize: "cover", backgroundImage: `url("img/theatre.jpg")` }}>
-      <Movielist slideFavoriteLeft={slideFavoriteLeft} slideFavoriteRight={slideFavoriteRight} />
+      <Movielist star={star} setStar={setStar} saveMovie={saveMovie} banner={setBanner} setBanner={setBanner} slideFavoriteLeft={slideFavoriteLeft} slideFavoriteRight={slideFavoriteRight}  favorites={favorites} setFavorites={setFavorites} useState ={useState}/>
       <Favorites slideFavoriteLeft={slideFavoriteLeft} slideFavoriteRight={slideFavoriteRight} favorites={favorites} setFavorites={setFavorites} useState ={useState}/>
       {/* <Favorites slideFavoriteLeft={slideFavoriteLeft} /> */}
       {/* <Movielist /> */}
--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -8,13 +8,13 @@ import { TbHeartOff } from 'react-icons/tb'
 
 
 
-function Movielist({slideFavoriteLeft, slideFavoriteRight}) {
+function Movielist({saveMovie, star, setStar, banner, setBanner, favorites, setFavorites}) {
   const [name, setName] = useState([]);
   const [person, setPerson] = useState("")
   // const [count, setCount] = useState(0)
   const [value, setValue] = React.useState(0);
-  const [star, setStar] = useState(null);
-  const [banner, setBanner] = useState("");
+  // const [star, setStar] = useState(null);
+  
 
 
 
@@ -66,7 +66,7 @@ function Movielist({slideFavoriteLeft, slideFavoriteRight}) {
     setSee(i);
   };
   const [searchTerm, setSearchTerm] = useState('');
-  const [favorites, setFavorites] = useState([]);
+  // const [favorites, setFavorites] = useState([]);
   const {
     register,
     handleSubmit,
@@ -111,39 +111,7 @@ function Movielist({slideFavoriteLeft, slideFavoriteRight}) {
   }
 
 
-  const saveMovie = (poster, title) => {
 
-    const r = favorites.some(i => i.movPoster.includes(poster, title));
-    // console.log("Hello");
-
-    // (!favorites[item].movPoster === yolo) 
-    if (!r) {
-      setBanner("Movie added!")
-      setTimeout(() => setBanner(""), 2000);
-      setFavorites((film) => {
-        const shows = {
-          id: film.length === 0 ? 1 : film[film.length - 1].id + 1,
-          movPoster: poster,
-          movTitle: title
-        }
-        // alert("movie added to favorites!")
-        // console.log(shows.movTitle);
-        // console.log(shows.movPoster);
-        // console.log(shows);
-        //  setStar(shows.movPoster);
-
-        const returnValue = [...film, shows];
-        return returnValue;
-
-
-      });
-    }
-    else {
-      setBanner("Movie already exists in your favorites")
-      setTimeout(() => setBanner(""), 2000);
-    }
-
-  }
 
   return (
 
