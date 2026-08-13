02160c54711183e9137b2a17e13dbf543c12eb1b
Steve Dytewski <dy2ski@gmail.com>
2026-08-13 16:20:42 -0700

    Started creating a test for searching for titles that are a match of my search

---
+++ tests/e2e/movie-search-title.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { MovieListPage } from '../pages/movie-list.page';
+
+test('finds the title of movies after search', async ({ page }) => {
+  const movieListPage = new MovieListPage(page);
+
+  await page.goto('http://localhost:3000');
+  await movieListPage.searchForMovie('battle');
+    
+  
+});
